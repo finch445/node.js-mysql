60bd4bc0e628f6d7ce019ea9c469aeb380926804
finch445 <b0442012@ems.niu.edu.tw>
2020-04-27 21:25:58 +0800

    create a table

---
+++ demo_create_table.js
@@ -0,0 +1,25 @@
+/*
+To create a table in MySQL, use the "CREATE TABLE" statement.
+Make sure you define the neme of the databse when you create the connection.
+
+Here is an example of create a table named "customers" .
+*/
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "william",
+	password: "nopassword",
+	database: "mydb"
+});
+
+con.connect(function (err) {
+	if (err) throw err;
+	console.log("Connected!!");
+	var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+	con.query(sql, function(err, result) {
+		if (err) throw err;
+		console.log("Table created!!");
+	});
+});
