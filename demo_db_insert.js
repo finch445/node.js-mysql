6935b2ae00431e074e91111d2e4950309b6d3df7
finch445 <b0442012@ems.niu.edu.tw>
2020-04-29 13:35:03 +0800

    insert data to table

---
+++ demo_db_insert.js
@@ -0,0 +1,20 @@
+//Insert a record in the "customers" table.
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "william",
+	password: "nopassword",
+	database: "mydb"
+});
+
+con.connect(function (err){
+	if (err) throw err;
+	console.log("connected!!");
+	var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+	con.query(sql, function(err, result) {
+		if (err) throw err;
+		console.log("1 record inserted.");
+	});
+})
