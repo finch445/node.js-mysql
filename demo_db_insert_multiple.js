8315a9d7f1fda194a27f7af2cc5436fdbff9f352
finch445 <b0442012@ems.niu.edu.tw>
2020-04-29 14:19:49 +0800

    insert multiple

---
+++ demo_db_insert_multiple.js
@@ -0,0 +1,37 @@
+//insert multiple value into a table.
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "william",
+	password: "nopassword",
+	database: "mydb"
+});
+
+con.connect(function (err){
+	if (err) throw err;
+	console.log("Connected!!");
+	var sql = "INSERT INTO customers (name, address) VALUES ?";
+	var values = [
+    ['John', 'Highway 71'],
+    ['Peter', 'Lowstreet 4'],
+    ['Amy', 'Apple st 652'],
+    ['Hannah', 'Mountain 21'],
+    ['Michael', 'Valley 345'],
+    ['Sandy', 'Ocean blvd 2'],
+    ['Betty', 'Green Grass 1'],
+    ['Richard', 'Sky st 331'],
+    ['Susan', 'One way 98'],
+    ['Vicky', 'Yellow Garden 2'],
+    ['Ben', 'Park Lane 38'],
+    ['William', 'Central st 954'],
+    ['Chuck', 'Main Road 989'],
+    ['Viola', 'Sideway 1633']
+  ];
+  //<query> 
+  con.query(sql, [values], function(err, result) {
+  	if (err) throw err;
+  	console.log("Number of records inserted: " + result.affectedRows);
+  });
+});
