3ce6208f3cb4c870a618ad0677615c8d4e1beadb
finch445 <b0442012@ems.niu.edu.tw>
2020-05-13 16:07:48 +0800

    order by name desc

---
+++ demo_db_orderby_desc.js
@@ -0,0 +1,20 @@
+// sort the result reverse alphabetically by name.
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "user",
+	password: "userpassword",
+	database: "mydb"
+});
+
+con.connect(function(err){
+	if (err) throw err;
+	console.log("Database connected!!")
+	con.query("SELECT * FROM customers ORDER BY name DESC", function(err, result){
+		if (err) throw err;
+		console.log(result);
+	});
+});
+
