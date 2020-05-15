abd19ecf39e6d204d53620dd1e2a27383c3e3981
finch445 <b0442012@ems.niu.edu.tw>
2020-05-15 12:15:01 +0800

    update a record.

--- demo_db_update.js
+++ demo_db_update.js
@@ -1,3 +1,6 @@
+//update existing records in a table by using the "UPDATE" statement.
+//Overwrite the address column from "Valley 345" to "Canyon 123"
+
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
